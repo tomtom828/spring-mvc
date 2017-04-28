ea4f10df7d872dbd26d466f682f22433802372ef
tomtom28 <tomthompson828@gmail.com>
2017-04-28 15:46:33 -0400

    added to readme"

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Spring MVC
-Template for a Java Spring MVC project using IntelliJay and Maven
+A template for a Java Spring MVC project using IntelliJ and Maven
 
+After cloning this repo down, open it from the pom.xml file in IntelliJ to download all dependencies.
+
+When running the Application in IntelliJ, open up to localhost:8080/ to see the served webpage.
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
+    @RequestMapping("/")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
